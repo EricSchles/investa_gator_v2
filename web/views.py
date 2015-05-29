b8ccc6f7667144d9fd0b9ff70860433818f77d04
EricSchles <ericschles@gmail.com>
2015-05-29 13:23:00 -0700

    frontend_stuff

--- web/views.py
+++ web/views.py
@@ -4,6 +4,8 @@ import pickle
 from crawler import Scraper
 from web import app
 
+scraper = Scraper()
+
 @app.route("/",methods=["GET","POST"])
 def index():
     #connections found between ads
@@ -12,52 +14,25 @@ def index():
 
 @app.route("/run",methods=["GET","POST"])
 def run():
-    scraper = Scraper()
     data = scraper.scrape(auto_learn=True)
     return redirect(url_for("index"))
 
 @app.route("/investigate",methods=["GET","POST"])
 def investigator():
-    password = request.form.get("password")
-    running = request.form.get("long_running")
-    if password == "like_i_d_tell_you":
-        if running == "long":
-            investigate.run()
-        else:
-            investigate.run(long_running=False)
+    scraper.investigate()
+    
     return redirect(url_for("index"))
 
 @app.route("/add",methods=["GET","POST"])
 def add():
     return render_template("add.html")
 
-@app.route("/add_numbers",methods=["GET","POST"])
-def add_numbers():
-    numbers = pickle.load( open("numbers.p","rb") )
-    network = request.form.get("network")
-    number = request.form.get("number")
-    if not network in numbers.keys():
-        numbers[network] = []
-    numbers[network].append(number)
-    return redirect(url_for("index"))
-
 @app.route("/add_data",methods=["GET","POST"])
 def add_data():
     investigation_type = request.form.get("investigation_type")
     url = request.form.get("url_list")
     urls = url.split(",")
-    scraper = Scraper()
-    data = scraper.scrape(links=urls,auto_learn=True)
-    if investigation_type == "directed":
-        keywords = pickle.load( open("keywords.p", "rb"))
-        for datum in data:
-            keywords += datum["new_keywords"]
-        pickle.dump(keywords, open("keywords.p","wb"))
-    elif investigation_type == "undirected":
-        train = pickle.load( open("train.p","rb"))
-        for datum in data:
-            train.append((datum["text_body"],"trafficking"))
-        pickle.dump( train, open("train.p","wb") )
+    print scraper.initial_scrape(links=urls)
     return redirect(url_for("index"))
 
 #add routes for visualization of existing datasources
